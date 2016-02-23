45a49f6f76e1c5decb04d4f7cc0c71a3ef7e8b4a
WilliamBurg <william.r.burger.17@dartmouth.edu>
2016-02-23 17:01:36 -0500

    More progress
    2/23/16

--- main.cpp
+++ main.cpp
@@ -8,48 +8,249 @@
 
 #include <iostream>
 #include <string>
+#define style_length 20
 using namespace std;
-
-
-class side_dish{
-public:
+//This is the base class for an item on the menu. It will bequeath thsese items to side dish, main dish, and drink classes through inheritance. The menus will be a linked list: the elements link to a previous and next item. Functions consist of setting member elements, printing member elements, and returning member elements or pointers to copies of those elements.
+class menu_item{
+protected:
+    menu_item* previous_item;
     string* ingredients;
+    int num_ingredients;
     char* name;
-    char* style;
+    char style[style_length];
     float price;
     int for_how_many;
     int calories;
-    side_dish();
-    ~side_dish();
+    menu_item* next_item;
+public:
+    menu_item();
+    ~menu_item();
+    string* get_ingredients_pointer(){
+        string* ingred = ingredients;
+        return ingred;
+    }
+    void set_ingredients();
+    void set_ingredients(string*,int);
+    void print_ingredients();
+    char* get_name(){
+        char* nam;
+        nam = new char[strlen(name)+1];
+        return nam;
+    }
+    void set_name();
+    void set_name(string);
+    void set_name(char*);
+    void print_name();
+    void set_style();
+    void print_style();
+    string get_style(){
+        return style;
+    };
+    void set_price(){
+        cout<< "Enter a price >>";
+        cin >> price;
+    }
+    void print_price(){
+        cout << price;
+    }
+    float get_price(){
+        return price;
+    }
+    void set_for_how_many(){
+        cout << "Enter how many people the dish is for >>";
+        cin >> for_how_many;
+    }
+    void print_for_how_many(){
+        cout << for_how_many;
+    }
+    int get_for_how_many(){
+        return for_how_many;
+    }
+    void set_calories(){
+        cout << "Enter how many calories the dish is >>";
+        cin >> calories;
+    }
+    void print_calories(){
+        cout << calories;
+    }
+    int get_calories(){
+        return calories;
+    }
 };
 
+//Function definitions that were not defined in line.
+string print_style_options(){
+    string sty;
+    cout << "chinese\n" << "diner\n" << "mexican\n" << "thai\n" << "american comfort\n" << "other\n";
+    cout << "Please type the style of the restaurant from the list >>";
+    getline(cin,sty);
+    while(sty.compare("chinese")!=0 and sty.compare("diner")!=0 and sty.compare("thai")!=0 and sty.compare("american comfort")!=0 and sty.compare("other")!=0){
+        cout <<"INVALID ENTRY PLEASE TRY AGAIN\n";
+        cout <<"chinese\n" << "diner\n" << "mexican\n" << "thai\n" << "american comfort\n" << "other\n";
+        cout << "Please type the style of the restaurant from the list >>";
+        getline(cin,sty);
+    }
+    return sty;
+}
+void menu_item::set_ingredients(){
+    int num,i=0;
+    string junk;
+    cout << "How many ingredients >>";
+    cin >> num;
+    ingredients = new string[num];
+    std::getline(cin,junk);
+    while (i<num){
+        cout << "Enter an ingredient >>";
+        std::getline(cin,ingredients[i]);
+        i++;
+    }
+    cout << "Ingredients update complete for side_dish ";
+    i=0;
+    while (i<strlen(name)){
+        cout<<name[i];
+        i++;
+    }
+    cout <<"\n";
+    num_ingredients = num;
+};
 
-side_dish::side_dish(){
+//The set_ingredients function without user input. It takes the pointer to the first element in the array of strings and also the number of ingredients in the array.
+void menu_item::set_ingredients(string* first_pointer,int num){
+    int i = 0;
+    num_ingredients = num;
+    ingredients = new string[num];
+    while(i<num){
+        ingredients[i]=first_pointer[i];
+        i++;
+    }
+}
+void menu_item::print_ingredients(){
+    int i=0;
+    while (i<num_ingredients){
+        cout << i+1 << ". " << ingredients[i] << "\n";
+        i++;
+    }
+}
+menu_item::menu_item(){
     ingredients = new string[1];
     ingredients[0] = "Unavailable";
     name = new char[7];
     strcpy(name,"holder");
-    style = new char[7];
     strcpy(style,"holder");
     price = 0.00;
     for_how_many = 0;
     calories = 0;
 }
 
-
-side_dish::~side_dish(){
-    delete name;
-    delete style;
+//Menu_item destructor will not need to delete name since it will be deleted in each child class
+menu_item::~menu_item(){
+}
+void menu_item::set_name(){
+    int i=0;
+    string nam;
+    cout << "Enter the side dish name >> ";
+    getline(cin,nam);
+    name = new char[nam.size()+1];
+    while (i<strlen(name)){
+        if(i<nam.size()){
+            name[i] = nam[i];
+        }
+        else
+            name[i]=' ';
+        i++;
+    }
+}
+//set_name and set_style functions can take either a pointer to the first character or the entire string
+void menu_item::set_name(char* first){
+    name = new char[strlen(first)+1];
+    strcpy(name,first);
+}
+void menu_item::set_name(string nam){
+    int i =0;
+    name = new char[nam.size()+1];
+    while (i<strlen(name)){
+        if(i<nam.size()){
+            name[i] = nam[i];
+        }
+        else
+            name[i]=' ';
+        i++;
+    }
+}
+void menu_item::print_name(){
+    int i = 0;
+    while(i<strlen(name)){
+        cout << name[i];
+        i++;
+    }
 }
 
 
-class contact{
+void menu_item::set_style(){
+    int i = 0;
+    string sty;
+    cout << "Select the style from the list below:\n";
+    sty = print_style_options();
+    while (i<strlen(style)){
+        if (i<sty.size()){
+            style[i] = sty[i];
+        }
+        else
+            style[i] = ' ';
+        i++;
+    }
+}
+void menu_item::print_style(){
+    int i = 0;
+    while(i<strlen(style)){
+        cout << style[i];
+        i++;
+    }
+}
+
+class side_dish:public menu_item{
 public:
-    int telephone;
+    side_dish();
+    ~side_dish();
+};
+side_dish::side_dish(){
+    ingredients = new string[1];
+    ingredients[0] = "Unavailable";
+    name = new char[7];
+    strcpy(name,"holder");
+    strcpy(style,"holder");
+    price = 0.00;
+    for_how_many = 0;
+    calories = 0;
+}
+side_dish::~side_dish(){
+    delete name;
+}
+
+class contact{
+private:
+    long telephone;
     char* address;
     char* website;
+public:
     contact();
     ~contact();
+    void set_telephone(){
+        cout << "Set the telephone number, no characters >>";
+        cin >> telephone;
+    }
+    long get_telephone(){
+        return telephone;
+    }
+    void print_telephone(){
+        cout << telephone;
+    }
+    void set_address();
+    char* get_address();
+    void print_address();
+    void set_website();
+    char* get_website();
+    void print_website();
 };
 contact::contact(){
     telephone = 0;
@@ -62,52 +263,85 @@ contact::~contact(){
     delete address;
     delete website;
 }
+void contact::set_address(){
+    int i=0;
+    string add;
+    cout << "Enter the address >> ";
+    getline(cin,add);
+    address = new char[add.size()+1];
+    while (i<strlen(address)){
+        if(i<add.size()){
+            address[i] = add[i];
+        }
+        else
+            address[i]=' ';
+        i++;
+    }
+}
+char* contact::get_address(){
+    char* add;
+    add = new char[strlen(address)+1];
+    return add;
+}
+void contact::print_address(){
+    int i = 0;
+    while(i<strlen(address)){
+        cout << address[i];
+        i++;
+    }
+}
+void contact::set_website(){
+    int i=0;
+    string add;
+    cout << "Enter the website >> ";
+    getline(cin,add);
+    website = new char[add.size()+1];
+    while (i<strlen(address)){
+        if(i<add.size()){
+            address[i] = add[i];
+        }
+        else
+            address[i]=' ';
+        i++;
+    }
+}
+char* contact::get_website(){
+    char* add;
+    add = new char[strlen(website)+1];
+    return add;
+}
+void contact::print_website(){
+    int i = 0;
+    while(i<strlen(website)){
+        cout << website[i];
+        i++;
+    }
+}
 
-
-
-
-class drink{
+class drink:public menu_item{
 public:
-    string* ingredients;
-    char* name;
-    char* style;
-    float price;
-    int for_how_many;
-    int calories;
     drink();
     ~drink();
 };
-
 drink::drink(){
     ingredients = new string[1];
     ingredients[0] = "Unavailable";
     name = new char[7];
     strcpy(name,"holder");
-    style = new char[7];
     strcpy(style,"holder");
     price = 0.00;
     for_how_many = 0;
     calories = 0;
 }
-
 drink::~drink(){
     delete name;
-    delete style;
 }
 
-
-
-
-class main_dish{
+class main_dish:public menu_item{
+private:
+    side_dish sidedish_pairing;
+    drink drink_pairing;
 public:
-    string* ingredients;
-    char* name;
-    char* style;
-    float price;
-    int for_how_many;
-    int calories;
-    side_dish* sidedish_pairings;
-    drink* drink_pairings;
     main_dish();
     ~main_dish();
 };
@@ -117,51 +351,48 @@ main_dish::main_dish(){
     ingredients[0] = "Unavailable";
     name = new char[7];
     strcpy(name,"holder");
-    style = new char[7];
     strcpy(style,"holder");
     price = 0.00;
     for_how_many = 0;
     calories = 0;
-    sidedish_pairings = new side_dish;
-    drink_pairings = new drink;
 }
-
 main_dish :: ~main_dish(){
     delete name;
-    delete style;
 }
 
-
-
 class menu{
-public:
+private:
     int num_dishes;
     int num_side_dishes;
     int num_drinks;
-    main_dish* main_dishes;
-    side_dish* side_dishes;
-    drink* drinks;
+    main_dish* main_dishes_head;
+    main_dish* main_dishes_tail;
+    side_dish* side_dishes_head;
+    side_dish* side_dishes_tail;
+    drink* drinks_head;
+    drink* drinks_tail;
+public:
     menu();
-    ~menu();
-    };
-
+    ~menu(){};
+};
 menu::menu(){
     num_dishes = 0;
     num_side_dishes=0;
     num_drinks=0;
-    main_dishes = nullptr;
-    side_dishes = nullptr;
-    drinks = nullptr;
-}
-
-menu::~menu(){
+    main_dishes_head = nullptr;
+    main_dishes_tail = nullptr;
+    side_dishes_head = nullptr;
+    side_dishes_tail = nullptr;
+    drinks_head = nullptr;
+    drinks_tail = nullptr;
 }
 
 class date{
-public:
+private:
     char day[3]; // mm/dd/yyyy
     char month[3];
     char year[5];
+public:
     date current_date(); // Is there a way how to get the date from the system?
     date();
 };
@@ -172,9 +403,10 @@ date::date(){
 }
 
 class hours{
-public:
+private:
     char open[6];  // hh:mm
     char close[6];  // hh:mm
+public:
     hours();
 };
 hours::hours(){
@@ -183,34 +415,42 @@ hours::hours(){
 }
 
 class seasonal_menu:public menu{
-    using menu::menu; //Inheriting constructor from menu, no default constructor necessary for this
+    using menu::menu;  //Inheriting constructor from menu, no default constructor necessary for this
     date date_starts;
     date date_ends;
 };
 
-//Random comment
 
 class restaurant{
-public:
+private:
     menu menu;
-    char* style;
+    char style[style_length];
     char* name;
     int distance;
     date date;
     hours hours;
     contact contact_info;
+public:
     restaurant();
+    ~restaurant();
 };
 restaurant::restaurant(){
-    style = new char[7];
     strcpy(style,"holder");
     name = new char[7];
     strcpy(name,"holder");
 }
+restaurant::~restaurant(){
+    delete name;
+}
+
 
 int main() {
-    restaurant mollys;
-    cout << mollys.menu.num_drinks;
+    side_dish meat;
+    string buff[2];
+    buff[0] = "tacos and such";
+    buff[1] = "I like big tacos";
+    meat.set_ingredients(buff,2);
+    meat.print_ingredients();
 }
 
 
